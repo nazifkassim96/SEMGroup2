af2efd410330a16d5cb3aa8fec5800a7f6391e79
AHMAD MUNAJAT BIN ZULKIPLI <81801592+MUNAJAT97@users.noreply.github.com>
2021-04-05 12:00:04 +0800

    Tukar false jadi true dan tambah password 'root'

--- library/configs.php
+++ library/configs.php
@@ -1,11 +1,11 @@
 <?php
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 define('DB_NAME', 'crs_db');
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-if($link === false){
+if($link === true){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
